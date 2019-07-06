fc683c932f05d17d58eca8ac118e7b786d818d87
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-07-06 10:42:21 -0400

    added basic file structure. Added Firebase CDN to index. Added Bootstrap CDN to index.

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDLGabsANQ8jKELPND_EQun-VAOGjD5R-c",
+    authDomain: "project-basil-db62e.firebaseapp.com",
+    databaseURL: "https://project-basil-db62e.firebaseio.com",
+    projectId: "project-basil-db62e",
+    storageBucket: "",
+    messagingSenderId: "107913695337",
+    appId: "1:107913695337:web:323e2e145c63685e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  var database = firebase.database();
