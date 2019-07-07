3a704e676df1a0a5183825af26c647edb5301b24
Madeleine Griggs <madeleinegriggs@gmail.com>
2019-07-07 11:55:04 -0400

    When user searches for recipes, food2fork api called and displays title and image of recipe. When user clicks on one recipe, the food2fork recipe api is called to retrieve the full recipe

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -17,23 +17,77 @@
 
 
 
-//   Food2Fork API Key: 6c25094e2b7ba0e57995415ce749ed94
+//   Food2Fork API Key (Main): 6c25094e2b7ba0e57995415ce749ed94
+//   Second Test API Key: b11d8301b0ecfac319569f557e520e48
+var key = "b11d8301b0ecfac319569f557e520e48"
 
-var searchTerm = "chicken"
-var queryURL = "https://www.food2fork.com/api/search?key=6c25094e2b7ba0e57995415ce749ed94&q=" + searchTerm;
 
-$.ajax({
+// Food2Fork Search API Call
+
+$("#recipe-search-btn").on("click", function() {
+    event.preventDefault();
+    searchTerm = $("#recipe-search").val();
+    var queryURL = "https://www.food2fork.com/api/search?key=" + key + "&q=" + searchTerm;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function(response) {
+        displayRecipes(response);
+      })
+})
+
+function displayRecipes(response) {
+  var results = JSON.parse(response);
+
+  recipeCount = results.count;
+  newRecipes = results.recipes;
+  for (i = 0; i < recipeCount; i ++) {
+    newDiv = $("<div>")
+    .attr("id", newRecipes[i].recipe_id);
+    newP = newRecipes[i].title;
+    newImage = $("<img>")
+    .attr("src", newRecipes[i].image_url);
+    newBreak = $("<br>");
+    newButton = $("<button>")
+    .attr("id", newRecipes[i].recipe_id)
+    .addClass("recipe-btn")
+    .text("Click here to see the recipe");
+    newDiv.append(newP, newBreak, newImage, newButton);
+    
+    $("#recipe-search-wrapper").append(newDiv);
+  }
+}
+
+function retrieveSingleRecipe() {
+  event.preventDefault();
+  recipeID = $(this).attr("id");
+
+  queryURL = "https://www.food2fork.com/api/get?key=" + key + "&rId=" + recipeID;
+
+  $.ajax({
     url: queryURL,
     method: "GET"
   })
   .then(function(response) {
-      var results = response;
-      console.log(JSON.stringify(results))
-  })
+    displaySingleRecipe(response)
 
+  })
+}
+
+function displaySingleRecipe(response) {
+  var results = JSON.parse(response);
+  console.log("The response result for single recipe is: " + results);
+}
+
+
+  // Testing firebase
   $(document).ready(function() {
     var name = "Let's see if that works.";
     database.ref().push({
         username: name,
     });
   })
+
+  $(document).on("click", ".recipe-btn", retrieveSingleRecipe)
